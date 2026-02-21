1fd5ff0ec4ba72374866a13797ec9f01f736e93c
smtemirlan <temirlansmagul2008@gmail.com>
2026-02-21 12:29:34 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

--- Practice 3/inheritance/super_functions.py
+++ Practice 3/inheritance/super_functions.py
@@ -1,9 +1,16 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname=fname
+    self.lastname=lname
+  def printname(self):
+    print(self.fistname, self.latname)
+
 #Example 1
-class Student('Person'):
+class Student(Person):
   def __init__(self, fname, lname):
     super().__init__(fname, lname)
 #Example 2
-class Student('Person'):
+class Student(Person):
   def __init__(self, fname, lname, year):
     super().__init__(fname, lname)
     self.graduationyear = year
